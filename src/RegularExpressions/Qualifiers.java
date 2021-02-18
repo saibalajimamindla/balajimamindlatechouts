d113ea2c908cab2a4f13aa8381999294bd208dfb
D1 <D1@TO-8K3KZL2-D.corp.techouts.local>
2021-02-18 13:20:30 +0530

    ...

--- src/RegularExpressions/Qualifiers.java
+++ src/RegularExpressions/Qualifiers.java
@@ -1,8 +1,28 @@
 package RegularExpressions;
 
+import java.util.regex.Pattern;
+
 public class Qualifiers {
 	public static void main(String[] args) {
+
+		System.out.println(Pattern.matches("[amn]?", "a"));
 		
+		System.out.println(Pattern.matches("[amn]?", "aaa"));
+		
+		System.out.println(Pattern.matches("[amn]?", "aammmnn"));
+		
+		System.out.println(Pattern.matches("[amn]?", "aazzta"));
+		
+		System.out.println(Pattern.matches("[amn]?", "am"));
+		
+		System.out.println(Pattern.matches("[amn]+", "a"));
+		
+		System.out.println(Pattern.matches("[amn]+", "aaa"));
+		
+		System.out.println(Pattern.matches("[amn]+", "aammmnn"));
+		
+		System.out.println(Pattern.matches("[amn]+", "aazzta"));
+
 	}
 
 }
