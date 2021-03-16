253d6f5c9eaf394e62168ff63ca81fcd0e44d2ae
D1 <D1@TO-8K3KZL2-D.corp.techouts.local>
2021-03-16 17:37:40 +0530

    16/03/2021

---
+++ src/csvtest/CsvWriteReadTest.java
@@ -0,0 +1,21 @@
+package csvtest;
+
+public class CsvWriteReadTest {
+	 
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+         
+        String fileName = System.getProperty("user.home")+"/student.csv";
+         
+        System.out.println("Write CSV file:");
+        CsvFileWriter.writeCsvFile(fileName);
+         
+        System.out.println("\nRead CSV file:");
+        CsvFileReader.readCsvFile(fileName);
+ 
+    }
+ 
+}
+
